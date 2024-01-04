82909baeabf7e13f286e74083f20c793920acf0d
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2024-01-04 19:23:03 +0000

    setup and rendered navbar

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import HomePage from './pages/home';
+import SearchPage from './pages/search';
+import PetDetailsPage from './pages/detail';
+import PetDetailsNotFound from './pages/petDetailsNotFound';
+import Root from './components/root';
+// Add react-router-dom imports
+import { RouterProvider } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
+import { createRoutesFromElements } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+
+// create router with JSX Route elements
+const appRouter = createBrowserRouter(
+  createRoutesFromElements(<Route path='/' element={<Root />} />)
+);
+
+function App() {
+  return <RouterProvider router={appRouter} />;
+}
+
+export default App;
